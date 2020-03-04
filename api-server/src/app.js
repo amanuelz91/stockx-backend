56c7c4dda879a5809c0beb7584aab5350e301b9a
amanuelZ <azeryihu@gmail.com>
2020-03-04 14:22:45 -0500

    removed unnecessary proxy in app.js

--- api-server/src/app.js
+++ api-server/src/app.js
@@ -8,24 +8,19 @@ const routes = require('./routes/routes')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     next();
   });
 
 app.use("/", routes)
-app.use((req, res, next) => {
-    req.query = new Proxy(req.query, {
-      get: (target, name) => target[Object.keys(target)
-        .find(key => key.toLowerCase() === name.toLowerCase())]
-    })
-  
-    next();
-  });
+
 app.get('/', (req, res) => {
     res.json({"message": "StockX API default endpoint"})
 })
 
+// Prevents spinning up new process tests in watch mode
 if(!module.parent){
     app.listen(3000, () => {
         logger.info("Server is listening on port", 3000)
