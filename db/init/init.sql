37b590e3fcc274efc22d88723c6f7ab486fb3482
amanuelZ <azeryihu@gmail.com>
2020-03-04 14:13:53 -0500

    removed unnecessary comments in init.sql

--- db/init/init.sql
+++ db/init/init.sql
@@ -1,5 +1,4 @@
 create database dockerstockx_dev;
 create database dockerstockx_test;
 create database dockerstockx_prod;
-/* \connect dockerstockx */
-/*create table truetosize (id SERIAL PRIMARY KEY, name VARCHAR NOT NULL, size REAL NOT NULL);*/
+
