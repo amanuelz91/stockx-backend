600e6a69aa7a4f2fb0e24bed3295cf5db580d1cf
amanuelZ <azeryihu@gmail.com>
2020-03-04 12:17:56 -0500

    deleted old files

--- api-server/setup.sh
+++ api-server/setup.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-# export PGUSER="amanuel"
-# export PGHOST="localhost"
-# export PGPASSWORD="amanuel"
-# export PGDATABASE="dockerstockx"
-# export PGPORT=5432
 ./node_modules/.bin/knex migrate:latest --env development
 echo Setting up Migrations
 sleep 3
